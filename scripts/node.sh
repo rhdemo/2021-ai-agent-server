9e2ec3b3a5b994cb2a4fa8c52591bfbf397c4cf4
Evan Shortiss <evanshortiss@gmail.com>
2021-03-01 14:42:20 +0000

    fix: docker local dev script

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,5 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 docker build -f Dockerfile.dev . -t summit-ai-agent-server
-
+npm install
 docker run --rm --net=infinispan-docker-compose_summit -p 3003:3003 -v "$(pwd)/src/:/usr/src/app/src/" --name=summit-ai-agent-server  summit-ai-agent-server
