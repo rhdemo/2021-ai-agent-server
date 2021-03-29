2de5880d6571a2441e1d3098ae3f1d43359e0b0f
Evan Shortiss <evanshortiss@gmail.com>
2021-03-29 18:38:28 +0100

    fix: push script image tag variable

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 printf "\n\n######## ai-agent-server push ########\n"
 
-IMAGE_REPOSITORY=${PHONE_SERVER_IMAGE_REPOSITORY:-quay.io/redhatdemo/2021-ai-agent-server:latest}
+IMAGE_REPOSITORY=${IMAGE_REPOSITORY:-quay.io/redhatdemo/2021-ai-agent-server:latest}
 
 echo "Pushing ${IMAGE_REPOSITORY}"
 docker push ${IMAGE_REPOSITORY}
